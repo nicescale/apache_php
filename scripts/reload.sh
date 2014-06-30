eeab35deda71f476a0f9c977f248fc6b522c74a9
William Hanwoody <hanwoody@gmail.com>
2014-06-30 18:45:56 +0800

    add supervisor for apache_php

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -9,4 +9,8 @@ get_sid() {
 }
 
 sid=`get_sid`
-/usr/local/bin/nicedocker exec $sid /usr/sbin/apachectl graceful
+[ -z "$sid" ] && echo "service id not found" 1>&2 && exit 1
+cid=`docker ps|grep $sid|awk '{print $1}'`
+docker kill -s USR1 $cid
+#/usr/local/bin/nicedocker exec $sid /usr/sbin/apachectl graceful
+#/usr/local/bin/nicedocker kill -s USR1 $sid
