da5f2cc9f982c09d3c05357b70122deb26aeb069
William Hanwoody <hanwoody@gmail.com>
2014-07-18 10:47:18 +0800

    support env for php

--- run.sh
+++ run.sh
@@ -2,5 +2,12 @@
 
 /usr/sbin/cron
 
+envfile=${CONF_DIR-:/etc/apache2}/env
+
+[ -f $envfile ] &&
+. $envfile &&
+eval `cat $envfile|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export`
+
 . /etc/apache2/envvars &&
+eval `cat /etc/apache2/envvars|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export` &&
 exec /usr/sbin/apache2 -DFOREGROUND
