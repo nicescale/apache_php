4f853861838595115478c401e0a87ab6b6f173af
William Hanwoody <hanwoody@gmail.com>
2014-07-21 09:10:24 +0800

    fix env

--- run.sh
+++ run.sh
@@ -9,5 +9,4 @@ envfile=${CONF_DIR-:/etc/apache2}/env
 eval `cat $envfile|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export`
 
 . /etc/apache2/envvars &&
-eval `cat /etc/apache2/envvars|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export` &&
 exec /usr/sbin/apache2 -DFOREGROUND
