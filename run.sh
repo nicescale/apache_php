19aee06d97c2d6dc850d2361d24f1527de6ae8b2
William Hanwoody <hanwoody@gmail.com>
2014-07-23 14:53:28 +0800

    fix apache watch failed problem

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ envfile=${CONF_DIR-:/etc/apache2}/env
 . $envfile &&
 eval `cat $envfile|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export`
 
-. /etc/apache2/envvars &&
+. /etc/apache2/envvars
+rm -f ${APACHE_RUN_DIR:-/var/run/apache2}/*
 exec /usr/sbin/apache2 -DFOREGROUND
